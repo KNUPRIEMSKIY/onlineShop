942666c73c89764c589fec87a6d437e49b9402aa
KNUPRIEMSKIY <zheniapriemskiy9h@gmail.com>
2022-05-29 16:26:30 +0300

    imports fix

--- backend/Seeder.js
+++ backend/Seeder.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import users from './data/users.js'
 import products from './data/products.js'
-import User from './models/userModel.js'
-import Product from './models/productModel.js'
-import Order from './models/orderModel.js'
+import User from './Models/userModel.js'
+import Product from './Models/productModel.js'
+import Order from './Models/orderModel.js'
 import connectDB from './Config/db.js'
 
 dotenv.config()
