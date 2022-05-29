aeab51683a9690c5f0fd8c6de284b3c9b79bea4a
KNUPRIEMSKIY <zheniapriemskiy9h@gmail.com>
2022-05-30 01:50:36 +0300

    w

--- backend/routess/productRoutes.js
+++ backend/routess/productRoutes.js
@@ -9,7 +9,7 @@ import {
   createProductReview,
   getTopProducts,
 } from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authmiddleware.js'
+import { protect, admin } from '../middleWare/authmiddleware.js'
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
 router.get('/top', getTopProducts)
